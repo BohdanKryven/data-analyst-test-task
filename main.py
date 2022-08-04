c73b61029f0932903677a19ea893d46ac501fc22
bohdan <bodyan1108@gmail.com>
2022-08-04 18:52:37 +0300

    added solution v2

--- main.py
+++ main.py
@@ -8,24 +8,39 @@ from datetime import timedelta
 from couchbase.auth import PasswordAuthenticator
 from couchbase.cluster import Cluster
 from couchbase.management.collections import CollectionManager
-from couchbase.options import ClusterOptions, ClusterTimeoutOptions, QueryOptions
-from couchbase.exceptions import MissingConnectionException, AmbiguousTimeoutException, QueryIndexNotFoundException
+from couchbase.options import (
+    ClusterOptions,
+    ClusterTimeoutOptions,
+    QueryOptions
+)
+from couchbase.exceptions import (
+    MissingConnectionException,
+    AmbiguousTimeoutException,
+    QueryIndexNotFoundException,
+    CouchbaseException
+)
+from couchbase.collection import Collection
+
+from handle_exceptions import handle_exception
 
 endpoint = "cb.kx-fpmfl9w8dlm-h.cloud.couchbase.com"
 username = "admin"
 password = "Admin_Modema1"
 bucket_name = "travel-sample"
 
-auth = PasswordAuthenticator(username, password)
-timeout_opts = ClusterTimeoutOptions(kv_timeout=timedelta(seconds=10))
-cluster = Cluster('couchbases://{}'.format(endpoint),
-                  ClusterOptions(auth, timeout_options=timeout_opts))
-cluster.wait_until_ready(timedelta(seconds=5))
+
+def cluster_():
+    auth = PasswordAuthenticator(username, password)
+    timeout_opts = ClusterTimeoutOptions(kv_timeout=timedelta(seconds=10))
+    cluster = Cluster('couchbases://{}'.format(endpoint),
+                      ClusterOptions(auth, timeout_options=timeout_opts))
+    cluster.wait_until_ready(timedelta(seconds=5))
+
+    return cluster
 
 
-def main():
-    if not cluster.connected:
-        raise MissingConnectionException()
+def get_scopes():
+    cluster = cluster_()
 
     connection = cluster.connection
 
@@ -35,7 +50,35 @@ def main():
     )
 
     scopes = manager.get_all_scopes()
-    print(scopes)
+    return scopes
+
+
+def create_primary_index(scope, collection):
+    cluster = cluster_()
+
+    try:
+        primary_index = f"CREATE PRIMARY INDEX ON `default`:`{bucket_name}`.{scope}.{collection}"
+        creating_primary_index = cluster.query(
+            primary_index
+        )
+        creating_primary_index.execute()
+    except CouchbaseException as ex:
+        handle_exception(ex)
+
+
+def add_column_collection(scope, collection):
+    cluster = cluster_()
+
+    test_column = f"UPDATE`{bucket_name}`.{scope}.{collection} SET testColumn = 'testData';"
+    add_test_column = cluster.query(
+        test_column,
+    )
+    add_test_column.execute()
+
+
+def main():
+    cluster = cluster_()
+    scopes = get_scopes()
 
     for scope in scopes:
         collections = scope.collections
@@ -43,27 +86,38 @@ def main():
 
         for collection in collections:
             print("Collection name: ", collection.name)
-            sql_query = f"SELECT * FROM `{bucket_name}`.{scope.name}.{collection.name}"
+            if not cluster.search_indexes():
+                create_primary_index(scope=scope.name, collection=collection.name)
+
+            sql_query = f"SELECT * FROM`{bucket_name}`.{scope.name}.{collection.name}"
 
             row_iter = cluster.query(
                 sql_query,
-                QueryOptions(metrics=True)
+                QueryOptions(metrics=True),
             )
-            res = []
+            if row_iter:
+                add_column_collection(scope=scope.name, collection=collection.name)
+
+            data = []
             try:
                 for row in row_iter.rows():
-                    res.append(row[collection.name])
+                    data.append(row[collection.name])
 
             except (
                     AmbiguousTimeoutException,
                     QueryIndexNotFoundException
             ) as ex:
-                print(ex)
+                handle_exception(ex)
                 continue
 
             os.makedirs(f"{bucket_name}/{scope.name}", exist_ok=True)
-            df = pd.DataFrame(res)
+            df = pd.DataFrame(data)
+            print(df.columns)
             df.to_csv(f"{bucket_name}/{scope.name}/{collection.name}.csv")
+
+
+def merge_csv():
+    pass
 
 
 if __name__ == '__main__':
